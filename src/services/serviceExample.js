09c9ac1d27cb9887b213219e56557882b0b76b66
shifat97 <shifatbinreza2@gmail.com>
2025-08-14 10:57:24 +0600

    remove: example files

--- src/services/serviceExample.js
+++
@@ -1,7 +0,0 @@
-import { Product } from "../models/index.js";
-
-export const createProduct = async (productData) => {
-  const newProduct = new Product(productData);
-  await newProduct.save();
-  return newProduct;
-};
